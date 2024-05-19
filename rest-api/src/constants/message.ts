cf531ccb3ec298b5a9e59684198f1fc27f5216da
davide.strianese <davide.strianese@itsvil.it>
2024-05-19 20:10:38 +0200

    dev: add constant file

---
+++ rest-api/src/constants/message.ts
@@ -0,0 +1,14 @@
+export const MESSAGEConstants = Object.freeze({
+    OPERATION_OK: 'Successful operation',
+    OPERATION_KO: 'Unsuccessful operation: ',
+    BAD_REQUEST: 'Bad request',
+    USER_NO_FOUD: 'User not found',
+    PASSWORD_KO: 'Invalid password',
+    LOGIN_OK: 'Valid credential',
+    EMAIL_USED: 'Email already used',
+    USER_NO_LOGIN: 'User not logged in',
+    USER_WRONG: 'Connected user does not match with the user to be deleted',
+    IS_OWNER_OK: 'Is owner pass',
+    IS_AUTHENTICATED: 'Is authenticated pass',
+    COOKIE_NOT_FOUND: 'Cookie not present'
+})
