71b3ad110120b28e55cebba703c70d74ee3634c7
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-05-28 12:44:45 +0200

    dev: add configuration schema for new address

--- dummy-ecommerce/src/schema/users.ts
+++ dummy-ecommerce/src/schema/users.ts
@@ -4,4 +4,13 @@ export const SignUpSchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6)
+});
+
+export const AddressSchema = z.object({
+    lineOne: z.string(),
+    lineTwo: z.string().nullable(),
+    pincode: z.string().length(6),
+    country: z.string(),
+    city: z.string(),
+    userId: z.number()
 })
