2b488425a06d5f8e17ce14589445ba9def3a530d
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-07-03 17:50:28 +0200

    dev: create API to insert todos

--- crud-api-ts-ex-mdb-zod-jest/src/api/todos/todos.handlers.ts
+++ crud-api-ts-ex-mdb-zod-jest/src/api/todos/todos.handlers.ts
@@ -1,10 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
-import { TodoWithId, Todos } from './todos.model';
+import { Todo, TodoWithId, Todos } from './todos.model';
+import { InsertOneResult } from 'mongodb';
 
 export async function findAll(req: Request, res: Response<TodoWithId[]>, next: NextFunction) {
   try {
     const todos = await Todos.find().toArray();
     res.json(todos);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function createOne(req: Request<{}, InsertOneResult, Todo>, res: Response<InsertOneResult>, next: NextFunction) {
+  try {
+    const validateResult = Todo.parse(req.body);
+    const insertResult = await Todos.insertOne(validateResult);
+    res.json(insertResult);
   } catch (error) {
     next(error);
   }
