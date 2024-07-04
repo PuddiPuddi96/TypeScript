7279815a5bcdd0f166e7be5b733ab7e6b02b07a6
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-07-04 12:11:44 +0200

    dev: add findOne function

--- crud-api-ts-ex-mdb-zod-jest/src/api/todos/todos.handlers.ts
+++ crud-api-ts-ex-mdb-zod-jest/src/api/todos/todos.handlers.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { Todo, TodoWithId, Todos } from './todos.model';
+import { ParamsWithId } from '../../interfaces/ParamsWithId';
+import { ObjectId } from 'mongodb';
 
 export async function findAll(req: Request, res: Response<TodoWithId[]>, next: NextFunction) {
   try {
@@ -19,6 +21,21 @@ export async function createOne(req: Request<{}, TodoWithId, Todo>, res: Respons
       _id: insertResult.insertedId,
       ...req.body,
     });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function findOne(req: Request<ParamsWithId, TodoWithId, {}>, res: Response<TodoWithId>, next: NextFunction) {
+  try {
+    const result = await Todos.findOne({
+      _id: new ObjectId(req.params.id),
+    });
+    if (!result) {
+      res.status(404);
+      throw new Error(`Todo with id "${req.params.id}" not found`);
+    }
+    res.json(result);
   } catch (error) {
     next(error);
   }
--- crud-api-ts-ex-mdb-zod-jest/src/api/todos/todos.routes.ts
+++ crud-api-ts-ex-mdb-zod-jest/src/api/todos/todos.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import * as TodoHandlers from './todos.handlers';
 import { Todo } from './todos.model';
 import { validateRequest } from '../../middlewares';
+import { ParamsWithId } from '../../interfaces/ParamsWithId';
 
 const router = Router();
 
 router.get('/', TodoHandlers.findAll);
+router.get('/:id', validateRequest({ params: ParamsWithId }), TodoHandlers.findOne);
 router.post('/', validateRequest({ body: Todo }), TodoHandlers.createOne);
 
 export default router;
